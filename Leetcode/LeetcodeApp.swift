478f2cb1d9332dede6efac96da5178c221c428ed
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-29 07:16:41 +0500

    Initial Commit

---
+++ Leetcode/LeetcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetcodeApp.swift
+//  Leetcode
+//
+//  Created by Muhammad Asad Chattha on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeetcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
