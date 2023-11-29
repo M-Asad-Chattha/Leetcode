aa6d1aa2e5441d53f5e704049979a934cdc7c5a0
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-29 08:15:53 +0500

    add: add Problem_1 file and get ready for leetcode-problem #1 "Two sum"

--- Leetcode/App/LeetcodeApp.swift
+++ Leetcode/App/LeetcodeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LeetcodeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Problem_1()
         }
     }
 }
---
+++ Leetcode/Problems/Problem-1.swift
@@ -0,0 +1,21 @@
+//
+//  Problem-1.swift
+//  Leetcode
+//
+//  Created by Muhammad Asad Chattha on 29/11/2023.
+//
+//  Problem-1: Two Sum
+//  Link: https://leetcode.com/problems/two-sum/
+//
+
+import SwiftUI
+
+struct Problem_1: View {
+    var body: some View {
+        Text("Hello Leetcode!")
+    }
+}
+
+#Preview {
+    Problem_1()
+}
