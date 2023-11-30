6e8256b26682a9fd13d9129b5afaa63071c4a1cb
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-30 22:53:53 +0500

    feat: solve Leetcode #1 "Two Sum"
    Problem: https://leetcode.com/problems/two-sum/description/
    My Submitted Solution: https://leetcode.com/problems/two-sum/solutions/4344184/find-pairs-fast-swift-solution-for-adding-up-to-a-target-number-in-arrays/

--- Leetcode/Problems/Problem-1.swift
+++ Leetcode/Problems/Problem-1.swift
@@ -11,10 +11,85 @@
 import SwiftUI
 
 struct Problem_1: View {
+
+    // MARK: - States
+    @State private var target: String = ""
+    @State private var numberInput: String = ""
+    @State private var numbers: [Int] = []
+    
+    @State private var alertMessage = ""
+    @State private var isShowingAlert = false
+    
     var body: some View {
-        Text("Hello Leetcode!")
+        List {
+            Section("Target") {
+                TextField("Enter Target", text: $target)
+                    .keyboardType(.numberPad)
+            }
+            
+            TextField("Enter Number to Add in Array", text: $numberInput)
+            
+            
+            Section("List") {
+                ForEach(0..<numbers.count, id: \.self) { num in
+                    Text(String(numbers[num]))
+                }
+            }
+        }
+        .onSubmit { appendItemToList() }
+        .alert("Indices for sum of \(target)", isPresented: $isShowingAlert) {
+            Button("ok"){}
+        } message: {
+            Text(alertMessage)
+        }
+        
+        Button("Find indices") {
+            let target = Int(target) ?? 0
+            let indices = twoSum(numbers, target)
+            alertMessage = "\(indices)"
+            isShowingAlert = true
+        }
+        .buttonStyle(.borderedProminent)
+        .disabled(sholdButtonDisable())
     }
 }
+
+// MARK: - Helper Methods
+extension Problem_1 {
+    func appendItemToList() {
+        if let number = Int(numberInput) {
+            withAnimation {
+                numbers.append(number)
+                numberInput = ""
+            }
+        }
+    }
+    
+    func sholdButtonDisable() -> Bool {
+        return (numbers.count >= 2 && !target.isEmpty) ?  false : true
+    }
+}
+
+
+// MARK: - Problem Solution
+extension Problem_1 {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var indexArray = [Int]()
+        
+    outerLoop: for (i, num) in nums.enumerated() {
+        for (j, num_1) in nums.enumerated() {
+            if (num + num_1 == target) && (i != j) {
+                indexArray.append(i)
+                indexArray.append(j)
+                break outerLoop
+            }
+        }
+        
+    }
+        return indexArray
+    }
+}
+
 
 #Preview {
     Problem_1()
